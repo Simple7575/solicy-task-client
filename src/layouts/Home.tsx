7a9da86e7eebc4f6bbc9b14c6fe68e7381acb69e
Simple <varg616@gmail.com>
2023-05-04 09:53:16 +0400

    after framer motion

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,12 +1,37 @@
 import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import PageLayout from "./PageLayout";
+import { AnimatePresence, motion } from "framer-motion";
+
+const pageVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+    },
+};
 
 export default function Home() {
     const navigate = useNavigate();
+    const { pathname } = useLocation();
 
     useEffect(() => {
         navigate("/accounts");
     }, [navigate]);
 
-    return <Outlet />;
+    return (
+        <AnimatePresence mode="popLayout">
+            <PageLayout>
+                <motion.div
+                    key={pathname}
+                    variants={pageVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <Outlet />
+                </motion.div>
+            </PageLayout>
+        </AnimatePresence>
+    );
 }
